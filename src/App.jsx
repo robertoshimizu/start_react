23f51a32a4f0b4d84132b628263acb202d430637
Roberto Shimizu <rshimizubr@gmail.com>
2023-11-04 19:51:49 -0300

    serach params

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { createRoot } from "react-dom/client";
-import Pet from "./Pet";
-
+import { createRoot } from 'react-dom/client'
+// delete Pet import, and add SearchParams
+import SearchParams from './SearchParams'
 
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="dog" breed="Havanese" />
-      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="cat" breed="Mix" />
+      {/* in App.jsx, replace all the Pets */}
+      <SearchParams />;
     </div>
-  );
-};
+  )
+}
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(<App />)
