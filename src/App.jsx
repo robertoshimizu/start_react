d8657ac82e71ef028168365cf973f8cea56e6fc1
Roberto Shimizu <rshimizubr@gmail.com>
2023-11-04 18:28:32 -0300

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
 const App = () => {
-    return <div>FarAway Travel</div>
+    return (
+        <div>
+            <Logo />
+            <Form />
+            <PackingList />
+            <Stats />
+        </div>
+    )
 }
+
+const Logo = () => <h1>🌴Far Away 💼</h1>
+
+const Form = () => {
+    return (
+        <div className="add-form">
+            <h3>What do you need for your trip?</h3>
+        </div>
+    )
+}
+
+const PackingList = () => <div className="list">LIST</div>
+
+const Stats = () => (
+    <footer className="stats">
+        <em>You have X items in your list, and you already packed X (X%)</em>
+    </footer>
+)
 
 export default App
