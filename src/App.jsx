f09a54acd063038aa1ea69c178726ae030554da7
Roberto Shimizu <rshimizubr@gmail.com>
2023-12-06 17:36:49 -0300

    Add Assets folder inside /src

--- src/App.jsx
+++ src/App.jsx
@@ -3,42 +3,42 @@ const pizzaData = [
       name: "Focaccia",
       ingredients: "Bread with italian olive oil and rosemary",
       price: 6,
-      photoName: "pizzas/focaccia.jpg",
+      photoName: "assets/pizzas/focaccia.jpg",
       soldOut: false,
     },
     {
       name: "Pizza Margherita",
       ingredients: "Tomato and mozarella",
       price: 10,
-      photoName: "pizzas/margherita.jpg",
+      photoName: "assets/pizzas/margherita.jpg",
       soldOut: false,
     },
     {
       name: "Pizza Spinaci",
       ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
       price: 12,
-      photoName: "pizzas/spinaci.jpg",
+      photoName: "assets/pizzas/spinaci.jpg",
       soldOut: false,
     },
     {
       name: "Pizza Funghi",
       ingredients: "Tomato, mozarella, mushrooms, and onion",
       price: 12,
-      photoName: "pizzas/funghi.jpg",
+      photoName: "assets/pizzas/funghi.jpg",
       soldOut: false,
     },
     {
       name: "Pizza Salamino",
       ingredients: "Tomato, mozarella, and pepperoni",
       price: 15,
-      photoName: "pizzas/salamino.jpg",
+      photoName: "assets/pizzas/salamino.jpg",
       soldOut: true,
     },
     {
       name: "Pizza Prosciutto",
       ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
       price: 18,
-      photoName: "pizzas/prosciutto.jpg",
+      photoName: "assets/pizzas/prosciutto.jpg",
       soldOut: false,
     },
   ];
@@ -65,6 +65,7 @@ const pizzaData = [
   }
   
   function Menu() {
+    
     const pizzas = pizzaData;
     // const pizzas = [];
     const numPizzas = pizzas.length;
@@ -107,7 +108,7 @@ const pizzaData = [
   }
   
   function Pizza({ pizzaObj }) {
-    console.log(pizzaObj);
+    
   
     // if (pizzaObj.soldOut) return null;
   
@@ -135,7 +136,7 @@ const pizzaData = [
     const openHour = 12;
     const closeHour = 22;
     const isOpen = hour >= openHour && hour <= closeHour;
-    console.log(isOpen);
+    
   
     // if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
     // else alert("Sorry we're closed");
